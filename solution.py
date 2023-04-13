da051641c82df28d87583ba7f99bbedf6610b59e
BurnFirework <99072007+BurnFirework@users.noreply.github.com>
2023-04-12 20:55:09 -0700

    updated solution

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import ttest_ind
+
 
 chat_id = 860138765
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:
+    imp_crit = 0.04
+    t, p_value = ttest_ind(x, y, equal_var=False)
+    return p_value < imp_crit
